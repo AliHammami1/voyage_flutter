ee8197d86b630abee009817a6e8f15ea2e0beb9c
AliHammami1 <alihammami2000@gmail.com>
2024-04-15 22:31:15 +0100

    version 3

--- lib/pages/meteo.page.dart
+++ lib/pages/meteo.page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:voyage/menu/drawer.widget.dart';
+import 'package:voyage/pages/meteo-details.page.dart';
 
 class MeteoPage extends StatelessWidget{
+  TextEditingController txt_ville = new TextEditingController();
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,13 +14,42 @@ class MeteoPage extends StatelessWidget{
           title: Text('Méteo Page'),
           backgroundColor: Colors.blueGrey,
         ),
-        body: Center(
-          child: Text(
-            'Méteo Page',
-            style: TextStyle(fontSize: 22, color: Colors.blueGrey),
-                ),
-        )
+        body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextFormField(
+              controller: txt_ville,
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.apartment),
+                  hintText: "ville",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(width: 1))),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size.fromHeight(50),
+                    backgroundColor: Colors.blueGrey),
+                onPressed: () {
+                  _onGetMeteoDetails(context, txt_ville.text);
+                },
+                child: Text(
+                  'chercher',
+                  style: TextStyle(fontSize: 22, color: Colors.white),
+                )),
+          ),
+        ],
+      ),
     );
   }
 
+}
+
+void _onGetMeteoDetails(BuildContext context, String ville) {
+  Navigator.push(context,
+      MaterialPageRoute(builder: (context) => MeteoDetailsPage(ville)));
 }
